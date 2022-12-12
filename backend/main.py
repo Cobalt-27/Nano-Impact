c40700d8bb01f04f7a1c35aa716fa6413da72b49
OsmiumD <1653187368@qq.com>
2022-12-12 22:03:58 +0800

    feat: Update router in main

--- backend/main.py
+++ backend/main.py
@@ -4,7 +4,8 @@ import asyncio
 import random
 from game import Game
 
-game=Game()
+game = Game()
+
 
 async def send(ws, type: str, data: str):
     print('>', type, data)
@@ -22,31 +23,17 @@ async def nethandle(websocket, path):
         await handle(websocket, type, data)
     # except Exception as e:
     #     print(repr(e))
-        # print('connection closed')
-
-
-def genmap(row, col) -> dict:
-    m = {}
-    m['Row'] = row
-    m['Col'] = col
-    m['Blocks'] = []
-    for i in range(row):
-        for j in range(col):
-            b = {}
-            b['Row'] = i
-            b['Col'] = j
-            b['Height'] = random.random()
-            b['Type'] = random.randint(0, 4)
-            m['Blocks'].append(b)
-    return m
+    # print('connection closed')
 
 
 async def handle(ws, type, data):
     global game
     game.clearbuf()
-    d=json.loads(data)
+    d = json.loads(data)
+    if type == 'NetGreet':
+        pass
     if type == 'NetStartGame':
-        game=Game()
+        game = Game()
         game.restart(d['SaveName'])
         # await send(ws, 'ServerSetMap', json.dumps(genmap(20, 10)))
     if type == 'NetUpgrade':
@@ -56,12 +43,27 @@ async def handle(ws, type, data):
         # await send(ws, 'ServerSetUnits', json.dumps([unit]))
     if type == 'NetEndRound':
         game.handle_endRound()
-    if type =='NetInteract':
-        game.handle_interact(d['From'],d['To'])
-    if type== 'NetMove':
-        game.handle_move(d['ID'],d['Row'],d['Col'])
-    for type,content in game.getbuf():
-        await send(ws,type,content)
+    if type == 'NetInteract':
+        game.handle_interact(d['From'], d['To'])
+    if type == 'NetMove':
+        game.handle_move(d['ID'], d['Row'], d['Col'])
+    if type == 'NetAssignRelic':
+        game.handle_assignRelic(d['ID'], d['Relic'])
+    if type == 'NetAddBuilding':
+        game.handle_addBuilding(d['Type'], d['Row'], d['Col'])
+    if type == 'NetEndRound':
+        game.handle_endRound()
+    if type == 'NetSave':
+        game.handle_save(d['Name'])
+    if type == 'NetQuit':
+        game.handle_quit()
+    if type == 'NetRollback':
+        game.handle_rollBack()
+    if type == 'NetRequestSaveInfo':
+        pass
+
+    for type, content in game.getbuf():
+        await send(ws, type, content)
 
 
 if __name__ == '__main__':
@@ -73,3 +75,18 @@ if __name__ == '__main__':
     loop.run_until_complete(
         websockets.serve(nethandle, ip, port))
     loop.run_forever()
+
+# def genmap(row, col) -> dict:
+#     m = {}
+#     m['Row'] = row
+#     m['Col'] = col
+#     m['Blocks'] = []
+#     for i in range(row):
+#         for j in range(col):
+#             b = {}
+#             b['Row'] = i
+#             b['Col'] = j
+#             b['Height'] = random.random()
+#             b['Type'] = random.randint(0, 4)
+#             m['Blocks'].append(b)
+#     return m
